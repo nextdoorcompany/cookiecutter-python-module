56429360197da1e658c25f3139f2d97861028e32
Justin Schechter <justin@nextdoorco.com>
2023-11-04 18:06:24 -0400

    Add ruff

--- {{ cookiecutter.project_slug }}/.dir-locals.el
+++ {{ cookiecutter.project_slug }}/.dir-locals.el
@@ -2,7 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((eval . (progn
-                   (setq-local compile-command "{{ cookiecutter.path_to_venv }}/bin/doit")))))
+                   (setq-local compile-command "doit")))))
  (python-mode . ((eval . (progn
                            (setq-local gud-pdb-command-name "{{ cookiecutter.path_to_venv }}/bin/python -m pdb")
                            (setq-local python-shell-virtualenv-root "{{ cookiecutter.path_to_venv }}"))))))
---
+++ {{ cookiecutter.project_slug }}/dodo.py
@@ -0,0 +1,110 @@
+from pathlib import Path
+
+
+DOIT_CONFIG = {
+    "default_tasks": [
+        #        "lint",
+        #        "mypy",
+        #        "test",
+        "ruff",
+        #        "coverage_with_fail_under",
+        #        "vulture",
+    ],
+    "continue": True,
+}
+
+
+cwd = Path(".")
+test_files = list(cwd.glob("test*.py"))
+python_files = list(cwd.glob("*.py"))
+
+
+# def task_lint():
+#     "Runs pylint on all Python files."
+#     for f in python_files:
+#         yield {
+#             "name": f.name,
+#             "actions": [
+#                 [
+#                     "{{ cookiecutter.path_to_venv }}/bin/python",
+#                     "-m",
+#                     "pylint",
+#                     "--output-format=parseable",
+#                     "--rcfile",
+#                     "pyproject.toml",
+#                     f,
+#                 ]
+#             ],
+#             "file_dep": [f],
+#         }
+
+
+# def task_test():
+#     "Runs pytest on all test files."
+#     for f in test_files:
+#         yield {
+#             "name": f.name,
+#             "actions": [["{{ cookiecutter.path_to_venv }}/bin/python", "-m", "pytest", f]],
+#             "file_dep": [f],
+#         }
+
+
+# def task_coverage():
+#     "Runs coverage over pytest on the test directory."
+#     return {
+#         "actions": [
+#             ["{{ cookiecutter.path_to_venv }}/bin/coverage", "run", "-m", "pytest", "."],
+#             ["{{ cookiecutter.path_to_venv }}/bin/coverage", "report"],
+#         ],
+#         "uptodate": [False],
+#         "verbosity": 2,
+#     }
+
+
+# def task_coverage_with_fail_under():
+#     "Runs coverage over pytest on the test directory with no output unless coverage is below threshold."
+#     return {
+#         "actions": [
+#             ["{{ cookiecutter.path_to_venv }}/bin/coverage", "run", "-m", "pytest", "."],
+#             ["{{ cookiecutter.path_to_venv }}/bin/coverage", "report"],
+#         ],
+#         "uptodate": [False],
+#     }
+
+
+# def task_vulture():
+#     "Runs vulture on the source directory."
+#     return {
+#         "actions": [
+#             ["{{ cookiecutter.path_to_venv }}/bin/vulture", "."],
+#         ],
+#         "uptodate": [False],
+#     }
+
+
+def task_ruff():
+    "Runs ruff on all Python files."
+    return {
+        "actions": [
+            ["ruff", "check", "."],
+        ],
+        "uptodate": [False],
+    }
+
+
+# def task_ruff_fix_imports():
+#     "Fixes import sorting using ruff for all Python files."
+#     for f in python_files:
+#         yield {
+#             "name": f.name,
+#             "actions": [
+#                 [
+#                     r"{{ cookiecutter.path_to_venv }}/bin/ruff",
+#                     "--fix",
+#                     "--select",
+#                     "I001",
+#                     f,
+#                 ]
+#             ],
+#             "file_dep": [f],
+#         }
