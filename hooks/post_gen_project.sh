40dbcb317c8db09a4fcb6e2a2ca4e5f913c9cde5
Justin Schechter <justin@nextdoorco.com>
2024-10-13 16:03:24 -0400

    Build the venv after creation

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/bash
 
 chmod +x {{ cookiecutter.main_file }}.py
+uv venv --python 3.13
+uv pip compile pyproject.toml --generate-hashes --output-file requirements.txt
+uv pip sync requirements.txt
 echo see https://github.com/nextdoorcompany/cookiecutter-python-module
