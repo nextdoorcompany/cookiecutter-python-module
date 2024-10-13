1d85b3349fcade039d4d9f87d105d9aeda179f2d
Justin Schechter <justin@nextdoorco.com>
2024-10-13 16:34:53 -0400

    Set color output in compilation environment

--- {{ cookiecutter.project_slug }}/.dir-locals.el
+++ {{ cookiecutter.project_slug }}/.dir-locals.el
@@ -2,7 +2,8 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((eval . (progn
-                   (setq-local compile-command "doit")))))
+                   (setq-local compile-command "doit")
+                   (setq-local compilation-environment '("FORCE_COLOR=1"))))))
  (python-mode . ((eval . (progn
                            (setq-local gud-pdb-command-name "{{ cookiecutter.path_to_venv }}/bin/python -m pdb")
                            (setq-local python-shell-virtualenv-root "{{ cookiecutter.path_to_venv }}"))))))
