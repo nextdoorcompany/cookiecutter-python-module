00c000ad8e1cc7585d415e9710b0ece4a43093c0
Justin Schechter <justin@nextdoorco.com>
2024-10-20 11:15:40 -0400

    Update docs

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,6 +2,12 @@
 
 chmod +x {{ cookiecutter.main_file }}.py
 uv venv --python 3.{{ cookiecutter.python_minor_version }} {{ cookiecutter.path_to_venv }}
-uv pip compile pyproject.toml --python {{ cookiecutter.path_to_venv }}/bin/python --generate-hashes --output-file requirements.txt
+uv pip compile pyproject.toml --python {{ cookiecutter.path_to_venv }}/bin/python --generate-hashes --quiet --output-file requirements.txt
 uv pip sync --python {{ cookiecutter.path_to_venv }}/bin/python requirements.txt
+echo
 echo see https://github.com/nextdoorcompany/cookiecutter-python-module
+echo
+doit list
+echo
+echo run ./{{ cookiecutter.main_file }}.py
+echo
