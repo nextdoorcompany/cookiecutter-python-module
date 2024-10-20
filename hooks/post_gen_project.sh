e02c8c9dcb81f0d5f6617fc399ca13aee28c05a5
Justin Schechter <justin@nextdoorco.com>
2024-10-20 10:38:47 -0400

    Use selected venv

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 
 chmod +x {{ cookiecutter.main_file }}.py
-uv venv --python 3.13
-uv pip compile pyproject.toml --generate-hashes --output-file requirements.txt
-uv pip sync requirements.txt
+uv venv --python 3.13 {{ cookiecutter.path_to_venv }}
+uv pip compile pyproject.toml --python {{ cookiecutter.path_to_venv }}/bin/python --generate-hashes --output-file requirements.txt
+uv pip sync --python {{ cookiecutter.path_to_venv }}/bin/python requirements.txt
 echo see https://github.com/nextdoorcompany/cookiecutter-python-module
