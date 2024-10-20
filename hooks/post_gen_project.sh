3bc59b00109a94e00615b5f13d59fb69d3a00400
Justin Schechter <justin@nextdoorco.com>
2024-10-20 10:44:54 -0400

    Select python minor version

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 
 chmod +x {{ cookiecutter.main_file }}.py
-uv venv --python 3.13 {{ cookiecutter.path_to_venv }}
+uv venv --python 3.{{ cookiecutter.python_minor_version }} {{ cookiecutter.path_to_venv }}
 uv pip compile pyproject.toml --python {{ cookiecutter.path_to_venv }}/bin/python --generate-hashes --output-file requirements.txt
 uv pip sync --python {{ cookiecutter.path_to_venv }}/bin/python requirements.txt
 echo see https://github.com/nextdoorcompany/cookiecutter-python-module
